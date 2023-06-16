d3e8927970d8c7471ab9c2271239a52874bf4053
Jaden J <Jtjester07@gmail.com>
2023-06-16 10:47:03 -0700

    endthy

--- Assets/Scripts/GetHit.cs
+++ Assets/Scripts/GetHit.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class GetHit : MonoBehaviour
 {
+    public GameObject prefabToSpawn;
+    public Vector2 spawnAreaSize = new Vector2(5f, 5f);
     private int health = 30;
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -14,6 +16,21 @@ public class GetHit : MonoBehaviour
                 Destroy(gameObject);
 
             Destroy(collision.gameObject);
+            SpawnPrefab();
         }
+    }
+
+    void SpawnPrefab()
+    {
+        Vector2 spawnPosition = GetRandomSpawnPosition();
+        Instantiate(prefabToSpawn, spawnPosition, Quaternion.identity);
+    }
+
+    Vector2 GetRandomSpawnPosition()
+    {
+        Vector2 parentPosition = transform.position;
+        float randomX = Random.Range(parentPosition.x - spawnAreaSize.x / 2, parentPosition.x + spawnAreaSize.x / 2);
+        float randomY = Random.Range(parentPosition.y - spawnAreaSize.y / 2, parentPosition.y + spawnAreaSize.y / 2);
+        return new Vector2(randomX, randomY);
     }
 }
